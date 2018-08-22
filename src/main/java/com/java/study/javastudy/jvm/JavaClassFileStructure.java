0a30955e544b17147801649e05095a96c778c826
lanjing <804288658@qq.com>
2018-08-22 22:58:53 +0800

    class文件结构学习

--- src/main/java/com/java/study/javastudy/jvm/JavaClassFileStructure.java
+++ src/main/java/com/java/study/javastudy/jvm/JavaClassFileStructure.java
@@ -1,4 +1,18 @@
 package com.java.study.javastudy.jvm;
 
+import java.io.InputStream;
+
+/**
+ * java class 文件构造学习
+ */
 public class JavaClassFileStructure {
+
+
+    public String name;
+    private Integer age;
+
+    public String getName(){
+        return this.name;
+    }
+
 }
