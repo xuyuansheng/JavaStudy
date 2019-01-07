5e8c51415549023209da1badb5ab85d08d1cf3be
lanjing <804288658@qq.com>
2019-01-07 20:15:59 +0800

    提交

--- src/main/java/com/java/study/javastudy/jvm/oom/JvmHeapOutOfMemoryError.java
+++ src/main/java/com/java/study/javastudy/jvm/oom/JvmHeapOutOfMemoryError.java
@@ -6,17 +6,18 @@ import java.util.List;
 /**
  * java堆内存溢出异常测试
  * VM Args：-Xms20m -Xmx20m -XX:+HeapDumpOnOutOfMemoryError
+ * @author xu
  */
-public class JVMHeapOutOfMemoryError {
+public class JvmHeapOutOfMemoryError {
 
     public static void main(String[] args) {
         List<Object> list = new ArrayList<>();
         while (true){
-            list.add(new MYObject());
+            list.add(new MyObject());
         }
     }
 
-    static class MYObject{
+    static class MyObject{
 
     }
 
