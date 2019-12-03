69a43c832397f13ab6bebb5bfad6c49ec0e09852
浮躁的菜 <804288658@qq.com>
2019-12-03 16:49:03 +0800

    提交

--- src/main/java/com/java/study/javastudy/xml/XmlUtils.java
+++ src/main/java/com/java/study/javastudy/xml/XmlUtils.java
@@ -1,5 +1,17 @@
 package com.java.study.javastudy.xml;
 
+import com.java.study.javastudy.xml.worldpayshengc.PaymentService;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 /**
  * @program: java-study
  * @description: xml
@@ -8,6 +20,38 @@ package com.java.study.javastudy.xml;
  */
 public class XmlUtils {
 
+    /**
+     * https://beta.developer.worldpay.com/docs/wpg/directintegration/3ds/
+     * http://developer-zh.worldpay.com/support/kb/gg/corporate-gateway-guide/content/directintegration/paymentrequests.htm
+     */
+
+    public static void main(String[] args) throws JAXBException, IOException {
+
+        String path = "C:\\Users\\xuyuansheng\\Desktop\\worldpay\\initerror.xml";
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        PaymentService paymentService = xmlStringToObject(new String(bytes), PaymentService.class);
+        System.out.println(paymentService);
+        System.out.println(objectToXmlString(paymentService));
+
+    }
+
+    public static String objectToXmlString(PaymentService paymentService) throws JAXBException {
+        JAXBContext jaxbContext = JAXBContext.newInstance(PaymentService.class);
+        StringWriter writer = new StringWriter();
+        Marshaller marshaller = jaxbContext.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+        marshaller.marshal(paymentService, writer);
+        String xmlStr = writer.toString();
+        return xmlStr;
+    }
+
+    public static <T> T xmlStringToObject(String xmlString, Class<T> clazz) throws JAXBException {
+        JAXBContext context = JAXBContext.newInstance(clazz);
+        Unmarshaller unmarshaller = context.createUnmarshaller();
+        Object object = unmarshaller.unmarshal(new StringReader(xmlString));
+        return (T) object;
+
+    }
 
 
 }
---
+++ src/main/java/com/java/study/javastudy/xml/worldpayshengc/WorldPayParamsValidateGroups.java
@@ -0,0 +1,17 @@
+package com.java.study.javastudy.xml.worldpayshengc;
+
+/**
+ * 校验分组
+ *
+ * @author Mr.Xu
+ * @date 2019/12/3 15:34
+ */
+public interface WorldPayParamsValidateGroups {
+    /**
+     * https://secure-test.worldpay.com/jsp/merchant/xml/paymentService.jsp
+     * 接口参数校验
+     */
+    interface InitPaymentValidate {
+    }
+}
+
