f649eed4985ad70ac9bb5df5435adb47edfaed3f
浮躁的菜 <804288658@qq.com>
2020-02-15 19:44:05 +0800

    Apollo本地测试 ApolloClient使用 和 结合springboot使用

---
+++ src/main/java/com/java/study/javastudy/apollo/ApolloTestController.java
@@ -0,0 +1,32 @@
+package com.java.study.javastudy.apollo;
+
+import com.ctrip.framework.apollo.Config;
+import com.ctrip.framework.apollo.spring.annotation.ApolloConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Mr.Xu
+ * @date 2020/2/15 18:19
+ */
+
+@RestController
+@RequestMapping("apolloTestController")
+public class ApolloTestController {
+    @ApolloConfig()
+    public Config apolloConfig;
+
+    @Autowired
+    public CustomConfigFromApollo customConfigFromApollo;
+
+    @RequestMapping("testClient")
+    public String testClient(String key, String application) {
+        return ApolloClientUtils.get(key, application);
+    }
+    @RequestMapping("testSpringBoot")
+    public String testSpringBoot(String key) {
+
+       return apolloConfig.getProperty(key, key)+"\n"+customConfigFromApollo.getKey();
+    }
+}
